72257827f87278fa6306057aa59c7fd2a6465e34
Shiba <robzun1000@gmail.com>
2022-01-04 04:35:23 +0100

    changed directory name and added section
    changed directory name for gh-pages and remade function to run multiple times

---
+++ docs/src/getoutinternetexplorer.js
@@ -0,0 +1,4 @@
+if (window.navigator.userAgent.includes("MSIE")) {
+    alert("For the love of god get a better browser");
+    window.location.replace("https://www.mozilla.org/en-US/firefox/new/");
+}
---
+++ docs/src/typing.js
@@ -0,0 +1,26 @@
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+
+// This is an array of html elements, since getElementsByClassName returns an array, since there can be more than one html element with that class.
+const elements = document.getElementsByClassName("typing-animation");
+
+(async (typedElements) => {
+   let texts = []
+   
+   for (let element of typedElements){
+    // The property to access the text inside the elements .innerHTML, if its a user interactuable element such as <input> or <textarea> then its .value
+    texts.push(element.innerHTML);
+    element.textContent = "";
+   }
+   
+   // texts and typedElements has the same length.
+   for (let i = 0; i < texts.length; i++) {
+      for (let character of texts[i]) {
+        typedElements[i].textContent += character;
+        await sleep(200)
+      } 
+   }
+  
+})(elements);
